5e7608c58faa91ab3baec5071357ea15f37ff546
Guilherme Garnier <guilherme.garnier@gmail.com>
2016-04-27 15:38:37 -0300

    Update React to 15.x

--- src/with-theme.js
+++ src/with-theme.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { mapProps, wrapDisplayName } from 'recompose'
-import createHelper from 'recompose/createHelper'
 
-const withTheme = (themes) => (BaseComponent, transform) => mapProps(
-  (props) => Object.assign({}, props, transform(themes[props.theme] || themes['default'])),
-  BaseComponent
-)
+const withTheme = (themes) => (BaseComponent, transform) =>
+  mapProps(
+    (props) => Object.assign({}, props, transform(themes[props.theme] || themes['default']))
+  )(BaseComponent)
 
-export default createHelper(withTheme, 'withTheme')
+export default withTheme
